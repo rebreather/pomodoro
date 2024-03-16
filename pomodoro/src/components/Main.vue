226dca2e82aaa92a4743085fca822edb2bbe9275
rebreather <yds04116@gmail.com>
2024-03-16 16:21:32 +0900

    chore: 메인 화면 초기화

---
+++ pomodoro/src/components/Main.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="main-view">
+        hi 
+    </div>
+</template>
+
+<script setup lang="ts">
+</script>
+
+<style scoped lang="scss">
+.main-view {
+    background-color: aliceblue;
+    width: 30rem;
+}
+</style>
