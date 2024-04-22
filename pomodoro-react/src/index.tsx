54abe16b8a7a37cde81b4606a37aae4ad8d80716
rebreather <yds04116@gmail.com>
2024-04-22 16:03:38 +0900

    chore: react로 개발환경 세팅

---
+++ pomodoro-react/src/index.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+// const root = document.createElement("div")
+// root.className = "container"
+// document.body.appendChild(root)
+
+// const rootDiv = ReactDOM.createRoot(
+//   document.getElementById('root') as HTMLElement
+// );
+
+// rootDiv.render(
+//     <App />
+// );
+
+const root = document.createElement("div")
+root.className = "container"
+document.body.appendChild(root)
+const rootDiv = ReactDOM.createRoot(root);
+rootDiv.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
