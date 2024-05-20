4d106855916b0ca8d6979f3b61cf1f0d7c6632a7
rebreather <yds04116@gmail.com>
2024-05-20 15:04:34 +0900

    feat: side menu 생성

---
+++ pomodoro-react/src/side-menu/SideMenu.tsx
@@ -0,0 +1,14 @@
+import './SideMenu.css';
+
+function Menu({setActiveContent, activeContent}: { setActiveContent: (content: string) => void, activeContent: string}) {
+
+    return (
+      <div className="menu-area">
+        <button className={`btn-timer ${activeContent === 'timer' ? 'active' : ''}`} onClick={() => setActiveContent('timer')}>Timer</button>
+        <button className={`btn-todo ${activeContent === 'todo' ? 'active' : ''}`} onClick={() => setActiveContent('todo')}>Todo</button>
+      </div>
+    );
+  }
+  
+  export default Menu;
+  
